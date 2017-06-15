0d1d7649fb42270f3bf7a9533520dfe2522ff199
ProfessorXZ <ssbiljan@gmail.com>
2017-06-16 00:13:55 +0200

    The patcher now searches the registry for the exe

--- TerrariaPatcher/Program.cs
+++ TerrariaPatcher/Program.cs
@@ -1,29 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Reflection;
 using Mono.Cecil;
+using Microsoft.Win32;
 
 namespace TerrariaPatcher
 {
 	class Program
 	{
-		private static readonly string InputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input");
 		private static readonly string OutputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output");
 
 		static void Main(string[] args)
 		{
-			Directory.CreateDirectory(InputDirectory);
 			Directory.CreateDirectory(OutputDirectory);
 
-			var terrariaPath = Path.Combine(InputDirectory, "Terraria.exe");
-			if (!File.Exists(terrariaPath))
+			string terrariaPath = null;
+			if (File.Exists("Terraria.exe"))
+			{
+				terrariaPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Terraria.exe");
+			}
+			else
+			{
+				RegistryKey key = Environment.Is64BitOperatingSystem
+					? RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64)
+					: RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+
+				var path = key.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 105600")
+					.GetValue("InstallLocation").ToString();
+				if (File.Exists(Path.Combine(path, "Terraria.exe")))
+				{
+					terrariaPath = Path.Combine(path, "Terraria.exe");
+				}
+			}
+
+			if (string.IsNullOrWhiteSpace(terrariaPath))
 			{
 				Console.WriteLine(
-					"Could not locate Terraria.exe. Place the exe into the 'input' directory and restart the patcher.");
+					"Could not locate Terraria.exe. Place the exe into the patcher's directory and restart the patcher.");
 			}
 			else
 			{
@@ -33,12 +45,7 @@ namespace TerrariaPatcher
 				socialApi.GetMethodDefinition("Initialize").Ignore();
 				socialApi.GetMethodDefinition("Shutdown").Ignore();
 
-				using (var stream = new MemoryStream())
-				{
-					assembly.Write(stream);
-					assembly.Write(Path.Combine(OutputDirectory, "Terraria.exe"));
-				}
-
+				assembly.Write(Path.Combine(OutputDirectory, "Terraria.exe"));
 				Console.WriteLine("Patching complete...");
 			}
 

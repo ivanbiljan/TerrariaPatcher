269e2826317fd133373bf912f45eda2b430a0919
ProfessorXZ <ssbiljan@gmail.com>
2017-06-15 22:00:07 +0200

    Initial commit

---
+++ TerrariaPatcher/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace TerrariaPatcher
+{
+	public static class Extensions
+	{
+		public static TypeDefinition GetTypeDefinition(this AssemblyDefinition assembly, string typeName)
+		{
+			return assembly.Modules.SelectMany(m => m.Types).First(t => t.Name == typeName);
+		}
+
+		public static MethodDefinition GetMethodDefinition(this TypeDefinition type, string methodName)
+		{
+			return type.Methods.First(m => m.Name == methodName);
+		}
+
+		public static void Ignore(this MethodDefinition method)
+		{
+			var body = method.Body;
+			var processor = body.GetILProcessor();
+			var target = body.Instructions[0];
+
+			processor.InsertBefore(target, Instruction.Create(OpCodes.Ret));
+		}
+	}
+}
---
+++ TerrariaPatcher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace TerrariaPatcher
+{
+	class Program
+	{
+		private static readonly string InputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input");
+		private static readonly string OutputDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output");
+
+		static void Main(string[] args)
+		{
+			Directory.CreateDirectory(InputDirectory);
+			Directory.CreateDirectory(OutputDirectory);
+
+			var terrariaPath = Path.Combine(InputDirectory, "Terraria.exe");
+			if (!File.Exists(terrariaPath))
+			{
+				Console.WriteLine(
+					"Could not locate Terraria.exe. Place the exe into the 'input' directory and restart the patcher.");
+			}
+			else
+			{
+				var assembly = AssemblyDefinition.ReadAssembly(terrariaPath);
+
+				var socialApi = assembly.GetTypeDefinition("SocialAPI");
+				socialApi.GetMethodDefinition("Initialize").Ignore();
+				socialApi.GetMethodDefinition("Shutdown").Ignore();
+
+				using (var stream = new MemoryStream())
+				{
+					assembly.Write(stream);
+					assembly.Write(Path.Combine(OutputDirectory, "Terraria.exe"));
+				}
+
+				Console.WriteLine("Patching complete...");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
